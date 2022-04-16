a67ad98ebc65a88cf6d2fb370b0c318cc626e740
terrylitianyu <terrylitianyu@hotmail.com>
2022-04-16 14:52:27 +0800

    upd

--- project/flaskProject/data_reader.py
+++ project/flaskProject/data_reader.py
@@ -20,7 +20,7 @@ class H1bDataReader:
     def get_df_shape(self):
         return self.df.shape
 
-    def attr_operator(self, attr, oper = "SUM", head = 0, others = False, filter_dict = {}):
+    def attr_operator(self, attr, oper = "SUM", head = 0, others = False, filter_dict = {}, drop_cases_val = 0):
         # unique_attrs = self.df[attr].unique()
         # print(unique_attrs)
 
@@ -36,6 +36,18 @@ class H1bDataReader:
 
         if oper == "SUM":
             opered_data = tmp_df[attr].reset_index(drop=True).value_counts()
+            opered_data = opered_data[opered_data > drop_cases_val]
+
+        if oper == "RATIO":
+            tmp_df = tmp_df.replace({"CASE_STATUS": ["CERTIFIED", "CERTIFIED-WITHDRAWN"]}, 1)
+            tmp_df = tmp_df.replace({"CASE_STATUS": ["DENIED", "WITHDRAWN"]}, 0)
+            tmp_df = tmp_df.groupby(attr).agg({"CASE_STATUS" : ["sum", "count"]})
+            tmp_df.columns = tmp_df.columns.droplevel(0)
+            tmp_df = tmp_df[tmp_df["count"] > drop_cases_val]
+
+            opered_data = tmp_df["sum"] / tmp_df["count"]
+            opered_data = opered_data.sort_values(ascending=False)
+
 
         rtn = opered_data
         if head > 0:
@@ -51,22 +63,34 @@ class H1bDataReader:
         print(rtn_dict)
         return rtn_dict
 
-    def salary_range(self, split = 50, max_bar = 350000):
+    def salary_range(self, split = 20, max_bar = 350000):
         salary_app_num_dict = {}
         salary_pass_rate_dict = {}
 
         salary_part_down = self.df[self.df["WAGE_RATE_OF_PAY_FROM"] <= max_bar]
         salary_part_up = self.df[self.df["WAGE_RATE_OF_PAY_FROM"] > max_bar]
-        salary_min = salary_part_down["WAGE_RATE_OF_PAY_FROM"].min()
-        salary_max = salary_part_down["WAGE_RATE_OF_PAY_FROM"].max()
+        # salary_min = salary_part_down["WAGE_RATE_OF_PAY_FROM"].min()
+        # salary_max = salary_part_down["WAGE_RATE_OF_PAY_FROM"].max()
 
-        for salary in np.linspace(salary_min, salary_max, split, endpoint=False):
+        salary_list = np.linspace(0, max_bar, split, endpoint=False)
+
+
+        for i in range(split):
+            salary = salary_list[i]
             tmp_df = salary_part_down[salary_part_down["WAGE_RATE_OF_PAY_FROM"] >= salary]
+            if i < split - 1:
+                tmp_df = tmp_df[tmp_df["WAGE_RATE_OF_PAY_FROM"] < salary_list[i + 1]]
             salary_app_num_dict[salary] = tmp_df.shape[0]
-            salary_pass_rate_dict[salary] = tmp_df[tmp_df["CASE_STATUS"] == "CERTIFIED"].shape[0] / tmp_df.shape[0]
+            salary_pass_rate_dict[salary] = 0
+            if tmp_df.shape[0] > 0:
+                salary_pass_rate_dict[salary] = (tmp_df[tmp_df["CASE_STATUS"] == "CERTIFIED"].shape[0] + \
+                                                tmp_df[tmp_df["CASE_STATUS"] == "CERTIFIED-WITHDRAWN"].shape[0]) / tmp_df.shape[0]
 
         salary_app_num_dict[max_bar] = salary_part_up.shape[0]
-        salary_pass_rate_dict[max_bar] = salary_part_up[salary_part_up["CASE_STATUS"] == "CERTIFIED"].shape[0] / salary_part_up.shape[0]
+        salary_pass_rate_dict[max_bar] = 0
+        if salary_part_up.shape[0] > 0:
+            salary_pass_rate_dict[max_bar] = (salary_part_up[salary_part_up["CASE_STATUS"] == "CERTIFIED"].shape[0] + \
+                                            salary_part_up[salary_part_up["CASE_STATUS"] == "CERTIFIED-WITHDRAWN"].shape[0]) / salary_part_up.shape[0]
 
         print(salary_app_num_dict, salary_pass_rate_dict)
         return salary_app_num_dict, salary_pass_rate_dict
@@ -91,6 +115,10 @@ class H1bDataReader:
         # self.df.dropna(subset = ["WAGE_RATE_OF_PAY_FROM"], inplace = True)
         self.df = self.df.astype({"WAGE_RATE_OF_PAY_FROM": float})
 
+    def casestate_preprocess(self):
+        self.df["CASE_STATUS"].replace("Certifi", "CERTIFIED", inplace = True)
+        self.df["CASE_STATUS"] = self.df["CASE_STATUS"].str.replace(' ', '').str.upper()
+
 
 if __name__ == "__main__":
     attr_list = {\
@@ -103,13 +131,15 @@ if __name__ == "__main__":
         "WAGE_RATE_OF_PAY_FROM": None,
         "WAGE_UNIT_OF_PAY": "Year"
     }
-    df_reader = H1bDataReader("../../data/h1b_data_2019.csv", attr_list = attr_list)
+    df_reader = H1bDataReader("../../data/h1b_data_2017.csv", attr_list = attr_list)
     df_reader.state_preprocess()
     df_reader.salary_preprocess()
+    df_reader.casestate_preprocess()
     df_reader.attr_operator("CASE_STATUS")
     df_reader.attr_operator("WORKSITE_STATE")
     df_reader.attr_operator("EMPLOYER_NAME", head = 10)
     df_reader.attr_operator("SOC_NAME", head = 10)
     df_reader.salary_range()
     df_reader.attr_operator("SOC_NAME", head = 10, filter_dict = {"WAGE_RATE_OF_PAY_FROM": {"GREATER": 100000, "LESS": 200000}})
+    df_reader.attr_operator("SOC_NAME", oper = "RATIO", head = 10, drop_cases_val = 1000)
 
